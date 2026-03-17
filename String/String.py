8c138d908517e8a3392a4aaaee4ff782112f19c8
keshavgarg-28 <keshav.garg@fissionlabs.com>
2026-03-17 15:20:15 +0530

    added comments

--- String/String.py
+++ String/String.py
@@ -1,16 +1,16 @@
 s= "python"
-print(s[::-1])
-print(s[:3])
+print(s[::-1]) ## reverse a string
+print(s[:3]) 
 
 t="   hello  "
-print(t.strip().upper())
+print(t.strip().upper()) ##strip-> to remove leading and trailing spaces
 
 a= "I love JAVA"
 print(a.replace("JAVA", "Python"));
 
 s = "apple,banana,grape"
-lst=s.split(",")
-print("-".join(lst))
+lst=s.split(",") ##split-> to convert str into list
+print("-".join(lst)) ## join-> to join list using any character
 
 s = "hello world python"
 lst=s.split(" ")
