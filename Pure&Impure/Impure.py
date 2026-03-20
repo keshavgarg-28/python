beb0052401668bb68f4b3d9b0df0c4004e880379
keshavgarg-28 <keshav.garg@fissionlabs.com>
2026-03-20 22:19:01 +0530

    Pure and Impure Fuction

---
+++ Pure&Impure/Impure.py
@@ -0,0 +1,10 @@
+lst=[1,2,3]
+def addlist(lst):
+    lst.append(4)
+print(addlist(lst))
+print(lst)
+
+x=5
+def add(a):
+    return x+a
+print(add(3))
---
+++ Pure&Impure/Pure.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return(a+b)
+print(add(2,3))
+
+list=[1,2,3]
+def addlist(list):
+    return list+[4]
+print(addlist(list))
