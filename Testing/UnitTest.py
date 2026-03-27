6cc387e753e6bdf47b5a2b87aebd45cfe1f4a901
keshavgarg-28 <keshav.garg@fissionlabs.com>
2026-03-27 11:46:05 +0530

    Testing

---
+++ Testing/UnitTest.py
@@ -0,0 +1,10 @@
+import unittest
+def add(a,b):
+    return a+b
+class Test(unittest.TestCase):
+    def test_add(self):
+        result=add(2,3)
+        self.assertEqual(result,5)
+
+if __name__ == "__main__":
+    unittest.main()
